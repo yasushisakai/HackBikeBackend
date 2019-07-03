26d3159ca4c8ca1fc9d3c68d9ec390001cfeb103
yasushisakai <ysshski@gmail.com>
2019-07-02 22:34:08 -0400

    refreshed

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
 mod handlers;
-mod model;
 
-use std::collections::HashMap;
 use std::env;
-use std::sync::{Arc, Mutex};
 
 use actix_web::http::{header};
-use actix_web::middleware::{cors::Cors, Logger, NormalizePath};
+use actix_web::middleware::{Logger, NormalizePath};
+use actix_cors::Cors;
 use actix_web::{web, App, HttpServer};
 use log::info;
 
-use handlers::{clear_table, get_table, deep_get, index, list_tables, set_module, set_table};
-use model::JSONState;
+use handlers::{index, get_data, set_data};
 
 fn main() -> std::io::Result<()> {
     if cfg!(debug_assertions) {
-        std::env::set_var("RUST_LOG", "actix_web=info,cs_cityio_backend=debug");
+        std::env::set_var("RUST_LOG", "actix_web=info,hackbike_backend=debug");
     } else {
-        std::env::set_var("RUST_LOG", "actix_web=info,cs_cityio_backend=info");
+        std::env::set_var("RUST_LOG", "actix_web=info,hackbike_backend=info");
     }
 
     env_logger::init();
@@ -28,11 +25,9 @@ fn main() -> std::io::Result<()> {
         Some(new_port) => port = new_port,
         None => port = "8080".to_string(),
     }
-    let hashmap: JSONState = Arc::new(Mutex::new(HashMap::new()));
 
     HttpServer::new(move || {
         App::new()
-            .data(hashmap.clone())
             .wrap(Logger::default())
             .wrap(NormalizePath)
             .wrap(
@@ -45,32 +40,12 @@ fn main() -> std::io::Result<()> {
                         header::CONTENT_TYPE,
                     ]),
             )
-            .service(web::resource("/api/table/{name}").route(web::get().to_async(get_table)))
-            .service(
-                web::resource("/api/table/update/{name}").route(web::post().to_async(set_table)),
-            )
-            .service(
-                web::resource("/api/table/update/{name}/").route(web::post().to_async(set_table)),
-            )
-            .service(
-                web::resource("/api/table/update/{name}/{module}").route(web::post().to_async(set_module)),
-            )
-            .service(
-                web::resource("/api/table/update/{name}/{module}/").route(web::post().to_async(set_module)),
-            )
-            .service(
-                web::resource("/api/table/clear/{name}").route(web::get().to_async(clear_table)),
-            )
-            .service(
-                web::resource("/api/table/clear/{name}/").route(web::get().to_async(clear_table)),
-            )
-            .service(web::resource("/api/tables/list/").route(web::get().to_async(list_tables)))
-            .service(web::resource("/api/tables/list").route(web::get().to_async(list_tables)))
-            .service(
-                web::resource("/api/table/{name}/{tail:.*}")
-                    .route(web::get().to_async(deep_get)),
-            )
-            .service(index)
+
+        ////////////////////////////////////////////////////////////////////////////////
+            .service(web::resource("/api/data").route(web::get().to_async(get_data)))
+            .service(web::resource("/api/data").route(web::post().to_async(set_data)))
+            .service(web::resource("/").route(web::get().to(index)))
+        ////////////////////////////////////////////////////////////////////////////////
     })
     .bind(format!("127.0.0.1:{}", &port))
     .and_then(|result| {
