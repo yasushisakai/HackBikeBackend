0ffff8c86712ccbd877a5acbb3d2d71a3c6dd4be
tkawahar <takashi.hiroshi.gml@gmail.com>
2019-07-07 15:38:25 -0400

    JSON output

--- src/handlers.rs
+++ src/handlers.rs
@@ -7,11 +7,8 @@ use futures::Stream;
 use actix_web::web;
 use std::io::prelude::*;
 use std::fs::File;
-use std::sync::{Arc, Mutex};
-use std::collections::HashMap;
-use serde_json::Value;
 
-pub type JSONState = Arc<Mutex<HashMap<String, Value>>>;
+use std::collections::BTreeMap;
 
 pub fn index() -> ActixResult<HttpResponse> {
     let mut file     = File::open("html/hello.html").unwrap();
@@ -26,16 +23,34 @@ pub fn index() -> ActixResult<HttpResponse> {
 // please reference https://github.com/CityScope/CS_CityIO_Backend for more examples
 
 pub fn get_data() -> impl Future<Item = HttpResponse, Error = Error> {
-    fut_ok(HttpResponse::Ok().json("get_data"))
+    // define JSON item in str
+    let data = r#"{"Name":"sensuikan1983", "Age":"110"}"#;
+    // convert str to JSON
+    let obj: BTreeMap<String, String> = serde_json::from_str(data).unwrap();
+    let objs = [obj.clone(), obj.clone()];
+    println!("{}, {:?}", data, obj);
+
+    // make body JSON contents with ROW JSON data
+    let mut contents = String::new();
+    contents = format!("{:?},\n{:?}", objs[0], objs[1]);
+    contents = "{\"data\":[\n".to_owned() + &contents + "\n]}";
+
+//    fut_ok(HttpResponse::Ok().json(objs))
+    fut_ok(HttpResponse::Ok()
+                    .content_type("application/json")
+                    .body(contents)
+                    )
+
 }
 
 pub fn set_data(
-    state: web::Data<JSONState>,
     pl: web::Payload,
 ) -> impl Future<Item = HttpResponse, Error = Error> {
     pl.concat2().from_err().and_then(move |body| {
-        println!("{:?}, {:?}", state, body);
-        fut_ok(HttpResponse::Ok().json("set_data"))
+        let data = std::str::from_utf8(&body).unwrap();
+        let obj: BTreeMap<String, String> = serde_json::from_str(data).unwrap();
+        println!("{}, {:?}", data, obj);
+        fut_ok(HttpResponse::Ok().json(obj))
     })
 }
 
